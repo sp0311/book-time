81c754cd4c2f3c26be99afc1fe03bbf105794a66
sp0311 <qploop@yahoo.co.jp>
2020-09-27 13:05:50 +0900

    ユーザー更新処理実装とそのリクエストテスト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params_update)
+      flash[:success] = "プロフィールが更新されました！"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def user_params_update
+    params.require(:user).permit(:name, :email, :introduction, :sex)
   end
 end
