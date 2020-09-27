71b275d2dbd61bb1eb07d48da2e995ac3a5edb53
sp0311 <qploop@yahoo.co.jp>
2020-09-28 00:23:20 +0900

    画像の拡張子とサイズの制限を設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,14 @@ class Book < ApplicationRecord
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
   validates :thoughts, length: { maximum: 400 }
+  validate  :picture_size
+
+  private
+
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "：5MBより大きい画像はアップロードできません。")
+    end
+  end
 end
