27215d08aaee10a8c21f8f18414e58c19469f22d
sp0311 <qploop@yahoo.co.jp>
2020-09-27 13:49:21 +0900

    フレンドリーフォワーディング実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   
-  def index
-  end
 
   def show
     @user = User.find(params[:id])
