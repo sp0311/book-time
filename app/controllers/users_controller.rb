6f76b78fdc25963e0fd8ee6c98a9622141572b98
sp0311 <qploop@yahoo.co.jp>
2020-09-27 14:51:57 +0900

    rubocop修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  
 
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
