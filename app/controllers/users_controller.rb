2f6bb6fc4ce2f64a87e1908f98b3f11610a55880
sp0311 <qploop@yahoo.co.jp>
2020-09-27 13:54:30 +0900

    ユーザー一覧ページ作成・リクエストテスト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   
 
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
   end
