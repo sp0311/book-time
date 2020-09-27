19e68fafacaff0fa11ad8a17349bf6c0ac854078
sp0311 <qploop@yahoo.co.jp>
2020-09-27 17:06:25 +0900

    Bookコントローラー作成・showアクション追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books.paginate(page: params[:page], per_page: 5)
   end
 
   def new
