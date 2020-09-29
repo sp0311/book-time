e086d8373dd24a641d42195d40caba2954159df4
sp0311 <qploop@yahoo.co.jp>
2020-09-30 02:22:40 +0900

    全ての本一覧ページ作成・検索機能実装

--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Book es", type: :system do
+RSpec.describe "Bookes", type: :system do
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
   let!(:book) { create(:book, :picture, user: user) }
